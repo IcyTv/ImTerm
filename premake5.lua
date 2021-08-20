3d7651f516101cb9a2614b4793a51d4d3e76138c
IcyTv <michael.finger@icytv.de>
2021-08-20 03:26:05 +0200

    Added premake5 file

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "ImTerm"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+        "include/imterm/*.hpp",
+        "include/imterm/*.tpp"
+	}
+
+	includedirs 
+	{
+		"include",
+        "../ImGui"
+	}
+
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
